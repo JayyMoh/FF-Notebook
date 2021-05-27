00bfdf8ee959b85522d6ba7bb3dae6b5c12abe7e
Jonathan <jonmohr@outlook.com>
2021-05-27 16:48:47 -0400

    Routes to the main pages work

--- server.js
+++ server.js
@@ -2,9 +2,24 @@ const express = require('express')
 const APP = express()
 const PORT = 3000
 
-// ===== Index View =====
+// ===== Main Page (index) =====
 APP.get('/ffnotebook', (req, res) => {
     res.render('index.ejs')
+})
+
+// ===== Veterans Page =====
+APP.get('/ffnotebook/veterans', (req, res) => {
+    res.render('veterans.ejs')
+})
+
+// ===== Rookie Page =====
+APP.get('/ffnotebook/rookies', (req, res) => {
+    res.render('rookies.ejs')
+})
+
+// ===== Devy Page =====
+APP.get('/ffnotebook/devy', (req, res) => {
+    res.render('devy.ejs')
 })
 
 // ===== New View =====
